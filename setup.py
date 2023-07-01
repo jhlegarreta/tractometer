90d1bca9b38ae636058fd9d55202e1f420dc527d
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-07-01 18:50:11 -0400

    ENH: Modernize package setup
    Modernize package setup:
    - Adopt PEP621 to specify prject metadata: host package static metadata
      and dependiencies in `pyproject.toml`.
    - Adopt PEP518 to specify minimum build system requirements for the
    package.
    
    Documentation:
    https://www.python.org/dev/peps/pep-0518/
    https://packaging.python.org/en/latest/specifications/declaring-project-metadata/#declaring-project-metadata
    
    Transition to `setuptools_scm` versioning system.
    
    Provide a non-empty version for downloaded source packaging cases.
    
    Change the package testing worfklow accordingly.
    
    Add a pre-commit configuration file to check style/format code.

--- setup.py
+++ setup.py
@@ -5,25 +5,12 @@ from glob import glob
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
-
-with open('requirements.txt') as f:
-    required_dependencies = f.read().splitlines()
-    external_dependencies = []
-    for dependency in required_dependencies:
-        if dependency[0:2] == '-e':
-            repo_name = dependency.split('=')[-1]
-            repo_url = dependency[3:]
-            external_dependencies.append('{} @ {}'.format(repo_name, repo_url))
-        else:
-            external_dependencies.append(dependency)
-
 ext_modules = [
     Extension(
         'challenge_scoring.tractanalysis.robust_streamlines_metrics',
         ['challenge_scoring/tractanalysis/robust_streamlines_metrics.pyx']
     )
 ]
-
 
 class CustomBuildExtCommand(build_ext):
     """Build_ext command to use when numpy headers are needed."""
@@ -51,24 +38,8 @@ class CustomBuildExtCommand(build_ext):
 
 
 opts = dict(
-    name='tractometer', version='1.0.1',
-    description='Scoring system used for the ISMRM 2015 Tractography Challenge',
-    url='https://github.com/scilus/ismrm_2015_tractography_challenge_scoring',
     ext_modules=ext_modules,
-    author='The challenge team',
-    author_email='jean-christophe.houde@usherbrooke.ca',
-    packages=[
-        'challenge_scoring',
-        'challenge_scoring.io',
-        'challenge_scoring.metrics',
-        'challenge_scoring.tractanalysis',
-        'challenge_scoring.utils'
-    ],
     cmdclass={'build_ext': CustomBuildExtCommand},
-    setup_requires=['Cython'],
-    scripts=glob('scripts/*.py'),
-    install_requires=external_dependencies,
-    requires=['numpy', 'scipy']
 )
 
 setup(**opts)
